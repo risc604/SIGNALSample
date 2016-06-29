7e9d0f2895935925d50efdbfbaab74b8ea385bfc
tomcat <tomcat.cheng@microlife.com.tw>
2016-06-29 16:16:45 +0800

    modify broadcastReceive event process

--- app/src/main/java/com/esignal/signaldemo/BluetoothLeService.java
+++ app/src/main/java/com/esignal/signaldemo/BluetoothLeService.java
@@ -283,38 +283,16 @@ public class BluetoothLeService extends Service
         // This is special handling for the Heart Rate Measurement profile.  Data parsing is
         // carried out as per profile specifications:
         // http://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
+        // For all other profiles, writes the data formatted in HEX.
+        final byte[] data = characteristic.getValue();
 
-        /*
-        if (UUID_HEART_RATE_MEASUREMENT.equals(characteristic.getUuid())) {
-            int flag = characteristic.getProperties();
-            int format = -1;
-            if ((flag & 0x01) != 0) {
-                format = BluetoothGattCharacteristic.FORMAT_UINT16;
-                Log.d(TAG, "Heart rate format UINT16.");
-            } else {
-                format = BluetoothGattCharacteristic.FORMAT_UINT8;
-                Log.d(TAG, "Heart rate format UINT8.");
-            }
-            final int heartRate = characteristic.getIntValue(format, 1);
-            Log.d(TAG, String.format("Received heart rate: %d", heartRate));
-            intent.putExtra(EXTRA_DATA, String.valueOf(heartRate));
-        } else
-        */
-        //{
-            // For all other profiles, writes the data formatted in HEX.
-            final byte[] data = characteristic.getValue();
-
-            if (data != null && data.length > 0)
-            {
-                final StringBuilder stringBuilder = new StringBuilder(data.length);
-                for (byte byteChar : data)
-                    stringBuilder.append(String.format("%02X", byteChar));
-                intent.putExtra(EXTRA_DATA, stringBuilder.toString());
-
-                //intent.putExtra(EXTRA_DATA, (Serializable) data);
-                //sendBroadcast(intent);
-            }
-        //}
+        if (data != null && data.length > 0)
+        {
+            final StringBuilder stringBuilder = new StringBuilder(data.length);
+            for (byte byteChar : data)
+                stringBuilder.append(String.format("%02X", byteChar));
+            intent.putExtra(EXTRA_DATA, stringBuilder.toString());
+        }
         sendBroadcast(intent);
     }
 
@@ -337,24 +315,16 @@ public class BluetoothLeService extends Service
             public void onTick(long millisUntilFinished)
             {
                 long tmp = millisUntilFinished/1000;
-                //if (tmp > 2)
-                //{
-                    Log.i(TAG, "Countdown second remaining: " + millisUntilFinished);
-                    bi.putExtra("countdown", tmp);
-                    //bi.putExtra("TimeOut", false);
-                    sendBroadcast(bi);
-                //}
-                //else if (tmp<=2)
-                //{
-                //    onFinish();
-                //}
+
+                Log.i(TAG, "Countdown second remaining: " + millisUntilFinished);
+                bi.putExtra("countdown", tmp);
+                sendBroadcast(bi);
             }
 
             @Override
             public void onFinish()
             {
                 bi.putExtra("countdown", 0L);
-                //bi.putExtra("TimeOut", true);
                 sendBroadcast(bi);
                 Log.i(TAG, "Timer finished");
             }
@@ -388,7 +358,6 @@ public class BluetoothLeService extends Service
     {
         // For API level 18 and above, get a reference to BluetoothAdapter through
         // BluetoothManager.
-
         if (mBluetoothManager == null)
         {
             mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
@@ -402,7 +371,6 @@ public class BluetoothLeService extends Service
         }
 
         mBluetoothAdapter = mBluetoothManager.getAdapter();
-
         if (mBluetoothAdapter == null)
         {
             Log.e(TAG, "Unable to obtain a BluetoothAdapter.");
@@ -443,8 +411,6 @@ public class BluetoothLeService extends Service
 
         //Connect Time Out Check  6 second
         handler.postDelayed(TimeOUTCheckTimer, BLE_CONNECT_TIMEOUT);
-
-        //cdt.start();
         return true;
     }
 
@@ -518,11 +484,6 @@ public class BluetoothLeService extends Service
 
         if(handler!=null)   handler.removeCallbacks(TimeOUTCheckTimer);
     }
-
-    //public void timerStart()
-    //{
-    //    cdt.start();
-    //}
 
     public void readCharacteristic(BluetoothGattCharacteristic characteristic,BluetoothGatt Gatt)
     {
